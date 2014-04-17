fef31839613868319cf542586612e00474f599fa
Dmitry Davydov <haudvd@gmail.com>
2014-04-17 15:26:47 +0400

    add gitignore, and worked functions

--- src/my_list.erl
+++ src/my_list.erl
@@ -1,1 +1,24 @@
 -module(my_list).
+-export([filter/2, map/2, quick_sort/1, flatten/1]).
+
+filter(Fun, List) ->
+    [X || X <- List, Fun(X)].
+     
+map(Fun, List) ->
+    [Fun(X) || X <- List].
+
+quick_sort([]) ->
+    [];
+quick_sort([OneEl]) ->
+    [OneEl];
+quick_sort([H | Tail]) ->
+    quick_sort([X || X <- Tail, X < H]) ++ [H] ++ quick_sort([X || X <- Tail, X >= H]).
+
+flatten(ListOfLists) when is_list(ListOfLists) ->
+    lists:reverse(flatten([], ListOfLists)).
+flatten(Acc, [H | Tail]) ->
+    flatten(flatten(Acc, H), Tail);
+flatten(Acc, []) ->
+    Acc;
+flatten(Acc, NoList) ->
+    [NoList | Acc].
