376bda1e0f90102109e82b801371ae23d1f87829
Dmitry Davydov <haudvd@gmail.com>
2014-04-17 14:02:14 +0400

    add tests and rebar config

---
+++ test/my_list_tests.erl
@@ -0,0 +1,18 @@
+-module(my_list_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+filter_test() ->
+    ?assertEqual([2,4,6], my_list:filter(fun(E) -> E rem 2 == 0 end, [1,2,3,4,5,6])).
+
+map_test() ->
+    ?assertEqual([2,4,6,8,10,12], my_list:map(fun(X) -> X * 2 end, [1,2,3,4,5,6])).
+
+%reduce_test() ->
+%    ?assertEqual([2,4,6,8,10,12], my_list:map(fun(X) -> X * 2 end, [1,2,3,4,5,6])).
+
+quick_sort_test() ->
+    ?assertEqual([1,2,3,4,5,6], my_list:quick_sort([4,5,1,3,2,6])).
+
+flatten_test() ->
+    ?assertEqual([1,2,3,4,5,6,7], my_list:flatten([[1,2],3,[4],[[5],6,[7]]])).
