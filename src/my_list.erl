d9db4d9905b64fad9f22c53ec521e7265daf5267
Dmitry Davydov <haudvd@gmail.com>
2014-04-17 15:33:23 +0400

    add reduce

--- src/my_list.erl
+++ src/my_list.erl
@@ -1,11 +1,16 @@
 -module(my_list).
--export([filter/2, map/2, quick_sort/1, flatten/1]).
+-export([filter/2, map/2, quick_sort/1, flatten/1, reduce/3]).
 
 filter(Fun, List) ->
     [X || X <- List, Fun(X)].
      
 map(Fun, List) ->
     [Fun(X) || X <- List].
+
+reduce(Fun, Acc, []) ->
+    Acc;
+reduce(Fun, Acc, [H | Tail]) ->
+    reduce(Fun, Fun(H, Acc), Tail).
 
 quick_sort([]) ->
     [];
--- test/my_list_tests.erl
+++ test/my_list_tests.erl
@@ -8,8 +8,8 @@ filter_test() ->
 map_test() ->
     ?assertEqual([2,4,6,8,10,12], my_list:map(fun(X) -> X * 2 end, [1,2,3,4,5,6])).
 
-%reduce_test() ->
-%    ?assertEqual([2,4,6,8,10,12], my_list:map(fun(X) -> X * 2 end, [1,2,3,4,5,6])).
+reduce_test() ->
+    ?assertEqual(21, my_list:reduce(fun(X, Sum) -> X + Sum end, 0, [1,2,3,4,5,6])).
 
 quick_sort_test() ->
     ?assertEqual([1,2,3,4,5,6], my_list:quick_sort([4,5,1,3,2,6])).
